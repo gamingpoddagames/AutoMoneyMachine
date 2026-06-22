8104355a54b50a32fba25abb39f2b37e0342569c
CoreFlow <gamingpoddagames@gmail.com>
2026-06-22 22:08:58 +0530

    Update news.py

--- modules/news.py
+++ modules/news.py
@@ -1,1 +1,15 @@
+import feedparser
 
+
+def get_news(url):
+    feed = feedparser.parse(url)
+
+    articles = []
+
+    for item in feed.entries:
+        articles.append({
+            "title": item.title,
+            "link": item.link
+        })
+
+    return articles
