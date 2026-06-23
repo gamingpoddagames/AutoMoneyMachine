5ded7815586424376ef12bb4c3fe80d84f3061f7
CoreFlow <gamingpoddagames@gmail.com>
2026-06-23 19:37:00 +0530

    Update routes.py

--- website/routes.py
+++ website/routes.py
@@ -1,1 +1,23 @@
+from flask import render_template
+import json
 
+
+def load_articles():
+    try:
+        with open("data/articles.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except:
+        return []
+
+
+def register_routes(app):
+
+    @app.route("/")
+    def home():
+
+        articles = load_articles()
+
+        return render_template(
+            "index.html",
+            articles=articles
+        )
