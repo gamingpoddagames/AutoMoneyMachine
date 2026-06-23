d691b55fefbb1d7bc839ff9a516f188598ad147e
CoreFlow <gamingpoddagames@gmail.com>
2026-06-23 18:54:49 +0530

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -1,1 +1,25 @@
+import sqlite3
 
+connection = sqlite3.connect("data/database.db")
+
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS news(
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+title TEXT,
+
+link TEXT,
+
+published TEXT
+
+)
+""")
+
+connection.commit()
+
+connection.close()
+
+print("Database Created Successfully")
