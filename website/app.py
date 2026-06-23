0407da33d45ec60db6af2c2087e730988833b89c
CoreFlow <gamingpoddagames@gmail.com>
2026-06-23 19:37:39 +0530

    Update app.py

--- website/app.py
+++ website/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+
+from website.routes import register_routes
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+register_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
