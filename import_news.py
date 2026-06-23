8c25308363794dd1124a8b328cd0ba0c6fd1417b
CoreFlow <gamingpoddagames@gmail.com>
2026-06-23 18:57:13 +0530

    Update import_news.py

--- import_news.py
+++ import_news.py
@@ -1,1 +1,33 @@
+import sqlite3
+from modules.news import get_news
 
+connection = sqlite3.connect("data/database.db")
+cursor = connection.cursor()
+
+feed = "https://feeds.bbci.co.uk/news/world/rss.xml"
+
+articles = get_news(feed)
+
+for article in articles:
+
+    cursor.execute("""
+
+    INSERT INTO news(title,link,published)
+
+    VALUES(?,?,?)
+
+    """,(
+
+        article["title"],
+
+        article["link"],
+
+        article["published"]
+
+    ))
+
+connection.commit()
+
+connection.close()
+
+print("News Imported Successfully")
