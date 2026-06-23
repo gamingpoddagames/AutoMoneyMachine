50fb13bff48e99e906c92ecbfa0db024a08753a8
CoreFlow <gamingpoddagames@gmail.com>
2026-06-23 18:42:00 +0530

    Update app.py

--- website/app.py
+++ website/app.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>AutoMoneyMachine</h1>
+    <h3>Website is Running Successfully!</h3>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
